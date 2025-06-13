e6414ac054ee7967a1e4147216393e36ff65e00f
nohh-a <nrod5432@hotmail.com>
2025-06-13 16:52:00 +1000

    Create functionality to add and delete  notes to array and screen.

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,12 +1,41 @@
-import React from "react";
+import React, {useState} from "react";
 
-function CreateArea() {
+function CreateArea(props) {
+
+    const [input, setInput] = useState({key:"", title:"",content:"",});
+    
+    function handleInput (event) {
+        const {value,name} = event.target;
+        setInput((prevInput) => ({
+            ...prevInput,
+            [name]: value,
+        } ));
+    }
+
+    function submitNote(event) {
+      props.onClick(input);
+      setInput({
+        key:"", title:"",content:"",
+      })
+      event.preventDefault();
+    }
+
   return (
     <div>
       <form>
-        <input name="title" placeholder="Title" />
-        <textarea name="content" placeholder="Take a note..." rows="3" />
-        <button>Add</button>
+        <input name="title" placeholder="Title" onChange={handleInput} value={input.title}/>
+        <textarea name="content" placeholder="Take a note..." rows="3" onChange={handleInput} value={input.content} />
+        <button onClick={submitNote}
+        // onClick={()=> {
+        //     props.onClick(input)
+        //     event.preventDefault()   
+        //     setNote({
+        //       key:"",
+        //       title:"",
+        //       content:"",
+        //     })        
+        // }}
+        >Add</button>
       </form>
     </div>
   );
--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,20 +1,52 @@
-import React from "react";
+import React, {useState} from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
-import notes from "../notes";
 import CreateArea from "./CreateArea";
 
+//1. Implement the add note functionality.
+//- Create a constant that keeps track of the title and content.
+//- Pass the new note back to the App.
+//- Add new note to an array.
+//- Take array and render seperate Note components for each item.
+
+//2. Implement the delete note functionality.
+//- Callback from the Note component to trigger a delete function.
+//- Use the filter function to filter out the item that needs deletion.
+//- Pass a id over to the Note component, pass it back to the App when deleting.
+
+//This is the end result you're aiming for:
+//https://pogqj.csb.app/
+
 function App() {
+    const [note, setNote] = useState([]);
+
+    function addNote(input) {
+        setNote((prevNotes)=> {
+            return [...prevNotes, input];
+        });
+    }
+
+    function deleteNote(id){   
+        setNote((prevNotes)=> {
+            return prevNotes.filter((note, index)=> {
+                return index!== id;
+            });
+        });
+    }  
     return (
         <div>
         <Header></Header>
-        <CreateArea></CreateArea>
-        {notes.map(noteMap => (
+        <CreateArea
+            onClick={addNote}    
+        ></CreateArea>
+        {note.map((noteMap, index) => (
             <Note
-            id = {noteMap.key}
+            id = {index}
+            key = {index}
             title = {noteMap.title}
             content = {noteMap.content}
+            onClick = {deleteNote}
         ></Note>
         ))}
         <Footer></Footer>
